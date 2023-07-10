a486899edbfa1a91376293d4342e44e0e987353c
Aadi Poddar <83405769+aadipoddar@users.noreply.github.com>
2023-07-10 09:37:40 +0000

    Project Setup

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Trello',
+  description: 'Trello with Chat GPT',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Trello</h1>
+    </main>
+  )
+}
